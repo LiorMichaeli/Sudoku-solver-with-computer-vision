c4a9117fad31dcf8a310d76daee7dc319f3a1225
Lior <liorm0995@gmail.com>
2023-09-10 22:51:55 +0300

    Add tests. All the tests passed.

--- tests.py
+++ tests.py
@@ -71,19 +71,19 @@ class MyTestCase(unittest.TestCase):
                                        [5, 9, 3, 7, 1, 8, 2, 4, 6]])
         self.assertEqual(expected_result, result)
 
-    '''def test_example4(self):
+    def test_example4(self):
         result = test_function('Data/sudoku_img_5.jpg', [9, 2, 3, 5, 6, 6, 8, 8, 4, 9, 2, 1, 6, 1, 9, 4, 1, 7, 8, 5,
                                                          6, 1, 8, 1, 7, 2, 3, 9, 5, 7])
-        expected_result = SudokuBoard([[1, 5, 9, 8, 3, 4, 5, 6, 9],
-                                       [9, 5, 6, 8, 2, 7, 3, 1, 4],
-                                       [1, 3, 4, 6, 9, 5, 7, 8, 2],
-                                       [4, 6, 9, 7, 3, 1, 8, 2, 5],
-                                       [5, 2, 8, 9, 6, 4, 1, 3, 7],
-                                       [7, 1, 3, 5, 8, 2, 4, 9, 6],
-                                       [2, 9, 7, 1, 4, 8, 6, 5, 3],
-                                       [6, 8, 5, 3, 7, 9, 2, 4, 1],
-                                       [3, 4, 1, 2, 5, 6, 9, 7, 8]])
-        self.assertEqual(expected_result, result)'''
+        expected_result = SudokuBoard([[8, 4, 9, 2, 1, 7, 3, 5, 6],
+                                       [6, 1, 3, 5, 9, 4, 2, 8, 7],
+                                       [2, 5, 7, 8, 3, 6, 9, 1, 4],
+                                       [9, 8, 5, 6, 4, 2, 1, 7, 3],
+                                       [7, 3, 6, 1, 8, 9, 4, 2, 5],
+                                       [4, 2, 1, 7, 5, 3, 6, 9, 8],
+                                       [5, 6, 2, 3, 7, 1, 8, 4, 9],
+                                       [1, 7, 4, 9, 6, 8, 5, 3, 2],
+                                       [3, 9, 8, 4, 2, 5, 7, 6, 1]])
+        self.assertEqual(expected_result, result)
 
 if __name__ == '__main__':
     unittest.main()
