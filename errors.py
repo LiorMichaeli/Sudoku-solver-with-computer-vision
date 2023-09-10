b212935fa3041b3da45e385baad26229356d0e54
Lior <liorm0995@gmail.com>
2023-09-10 12:01:38 +0300

    I Created errors file that includes errors msgs.

---
+++ errors.py
@@ -0,0 +1,11 @@
+
+# Imports
+from enum import Enum
+
+
+# Enum of errors.
+# Types of errors with the corresponding errors messages.
+class Errors(Enum):
+    ILLEGAL_VALUE_OF_SUDOKU_CUBE = "Error: Values of cubes in sudoku board must be integers between 0 and 9."
+
+
