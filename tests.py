fc8c76b2f9ad18a98738b2cba33dfa541919440b
Lior <liorm0995@gmail.com>
2023-10-02 21:55:06 +0300

    Add tests. All the tests passed.

--- tests.py
+++ tests.py
@@ -14,6 +14,7 @@ from sudoku_gui import SudokuGui
 from image_processing import get_sudoku_board_as_array_from_img
 import constants
 
+
 def test_function(arg_img_path: str, arg_initialized_numbers_in_sudoku_by_order: List[int]) -> SudokuBoard:
     try:
         sudoku_board_as_numbers_array = get_sudoku_board_as_array_from_img(arg_img_path,
@@ -23,10 +24,11 @@ def test_function(arg_img_path: str, arg_initialized_numbers_in_sudoku_by_order:
                                constants.LINE_COLOR)
         sudoku_solver = SudokuSolver(sudoku_board, sudoku_gui)
         sudoku_solver.solve_sudoku(arg_do_recursive_drawing=False)
-        # sudoku_gui.draw_sudoku_board(sudoku_board, arg_is_recursive_drawing=False)
+        #sudoku_gui.draw_sudoku_board(sudoku_board, arg_is_recursive_drawing=False)
         return sudoku_board
     except Exception as e:
         print("Error: ", e)
+
 
 class MyTestCase(unittest.TestCase):
     def test_example1(self):
@@ -44,6 +46,20 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(expected_result, result)
 
     def test_example2(self):
+        result = test_function('Data/sudoku_img_2.jpg', [1, 7, 2, 9, 8, 7, 4, 2, 4, 6, 5, 7, 9, 7, 1, 3, 4, 2, 8, 1, 6,
+                                                         2, 3, 1, 6, 4, 5, 3, 8])
+        expected_result = SudokuBoard([[5, 4, 1, 7, 3, 2, 9, 6, 8],
+                                       [8, 7, 6, 1, 9, 5, 3, 4, 2],
+                                       [3, 2, 9, 6, 4, 8, 1, 7, 5],
+                                       [6, 1, 3, 2, 5, 4, 7, 8, 9],
+                                       [7, 8, 5, 9, 1, 6, 4, 2, 3],
+                                       [4, 9, 2, 3, 8, 7, 6, 5, 1],
+                                       [9, 5, 7, 8, 6, 1, 2, 3, 4],
+                                       [2, 3, 8, 4, 7, 9, 5, 1, 6],
+                                       [1, 6, 4, 5, 2, 3, 8, 9, 7]])
+        self.assertEqual(expected_result, result)
+
+    def test_example3(self):
         result = test_function('Data/sudoku_img_3.jpg', [9, 6, 7, 3, 1, 4, 4, 1, 5, 3, 6, 8, 9, 6, 3, 1,
                                                          7, 1, 9, 4, 3, 1, 8, 9, 2, 7, 4, 4, 7])
         expected_result = SudokuBoard([[1, 5, 9, 8, 3, 4, 6, 7, 2],
@@ -57,7 +73,7 @@ class MyTestCase(unittest.TestCase):
                                        [9, 1, 5, 4, 6, 8, 2, 3, 7]])
         self.assertEqual(expected_result, result)
 
-    def test_example3(self):
+    def test_example4(self):
         result = test_function('Data/sudoku_img_4.jpg', [9, 1, 4, 5, 9, 5, 1, 3, 2, 5, 9, 6, 7, 8, 4, 3,
                                                          2, 3, 6, 5, 8, 6, 2])
         expected_result = SudokuBoard([[9, 3, 8, 4, 7, 6, 1, 5, 2],
@@ -71,7 +87,7 @@ class MyTestCase(unittest.TestCase):
                                        [5, 9, 3, 7, 1, 8, 2, 4, 6]])
         self.assertEqual(expected_result, result)
 
-    def test_example4(self):
+    def test_example5(self):
         result = test_function('Data/sudoku_img_5.jpg', [9, 2, 3, 5, 6, 6, 8, 8, 4, 9, 2, 1, 6, 1, 9, 4, 1, 7, 8, 5,
                                                          6, 1, 8, 1, 7, 2, 3, 9, 5, 7])
         expected_result = SudokuBoard([[8, 4, 9, 2, 1, 7, 3, 5, 6],
